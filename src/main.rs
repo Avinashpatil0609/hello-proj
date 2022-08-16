6034e4e4764f97d15b033061bc6e2e41de23fa57
Avinash <avp8860@icloud.com>
2022-08-16 13:02:51 +0530

    added get list method

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use serde::Deserialize;
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 use warp::body::json;
 use warp::{http, Filter};
-use serde::Deserialize;
+
 
 type Students = HashMap<String, String>;
 
@@ -39,8 +40,13 @@ async fn main() {
         .and(json())
         .and(store_filter.clone())
         .and_then(add_student_to_list);
+    let get_student = warp::get()
+        .and(warp::path("student"))
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and_then(get_students_from_list);
 
-    let routes = hello.or(add_student);
+    let routes = hello.or(add_student).or(get_student);
 
     warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 }
@@ -49,9 +55,27 @@ async fn add_student_to_list(
     student: Student,
     store: Store,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    store.student_list.write().await.insert(student.id, student.name);
+    store
+        .student_list
+        .write()
+        .await
+        .insert(student.id, student.name);
     Ok(warp::reply::with_status(
         "Added student to the list",
         http::StatusCode::CREATED,
+    ))
+}
+
+async fn get_students_from_list(store: Store) -> Result<impl warp::Reply, warp::Rejection>{
+    let mut result = HashMap::new();
+
+    let r = store.student_list.read().await;
+
+    for (key, value) in r.iter(){
+      result.insert(key, value);
+    }
+
+    Ok(warp::reply::json(
+        &result
     ))
 }
